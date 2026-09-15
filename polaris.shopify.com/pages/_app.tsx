03639443068883836c88dcbaa0226224c4f57e9b
gsouf <sghzal@gmail.com>
2026-09-15 14:10:39 +0200

    remove the boring modal about web-components

--- polaris.shopify.com/pages/_app.tsx
+++ polaris.shopify.com/pages/_app.tsx
@@ -1,22 +1,21 @@
-import type {AppProps} from 'next/app';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import {useEffect, StrictMode} from 'react';
-import {useRouter} from 'next/router';
+import { useEffect, StrictMode } from 'react';
+import { useRouter } from 'next/router';
 import '@shopify/polaris/build/esm/styles.css';
 import pkg from '../package.json';
 
-import {className} from '../src/utils/various';
-import {withBasePath} from '../src/utils/basePath';
+import { className } from '../src/utils/various';
+import { withBasePath } from '../src/utils/basePath';
 import Frame from '../src/components/Frame';
 import '../src/styles/globals.scss';
 import ViewTransition from '../src/components/ViewTransition';
-import InterstitialModal from '../src/components/InterstitialModal';
-import {useSafeDarkMode} from '../src/hooks/useSafeDarkMode';
+import { useSafeDarkMode } from '../src/hooks/useSafeDarkMode';
 
 // Remove dark mode flicker. Minified version of https://github.com/donavon/use-dark-mode/blob/develop/noflash.js.txt
 const noflash = `!function(){var b="darkMode",g="dark-mode",j="light-mode";function d(a){document.body.classList.add(a?g:j),document.body.classList.remove(a?j:g)}var e="(prefers-color-scheme: dark)",c=window.matchMedia(e),h=c.media===e,a=null;try{a=localStorage.getItem(b)}catch(k){}var f=null!==a;if(f&&(a=JSON.parse(a)),f)d(a);else if(h)d(c.matches),localStorage.setItem(b,c.matches);else{var i=document.body.classList.contains(g);localStorage.setItem(b,JSON.stringify(i))}}()`;
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const darkMode = useSafeDarkMode(false);
 
@@ -26,10 +25,9 @@ function MyApp({Component, pageProps}: AppProps) {
   // see https://stackoverflow.com/questions/61040790/userouter-withrouter-receive-undefined-on-query-in-first-render
 
   const ogImagePath = withBasePath(
-    `/og-images${
-      router.asPath === '/'
-        ? '/home'
-        : new URL(router.asPath, 'https://polaris.shopify.com').pathname
+    `/og-images${router.asPath === '/'
+      ? '/home'
+      : new URL(router.asPath, 'https://polaris.shopify.com').pathname
     }.png`,
   );
 
@@ -45,7 +43,7 @@ function MyApp({Component, pageProps}: AppProps) {
 
   return (
     <>
-      <script dangerouslySetInnerHTML={{__html: noflash}}></script>
+      <script dangerouslySetInnerHTML={{ __html: noflash }}></script>
 
       <Head>
         {/*
@@ -85,7 +83,6 @@ function MyApp({Component, pageProps}: AppProps) {
           </Frame>
         )}
       </div>
-      <InterstitialModal />
     </>
   );
 }
